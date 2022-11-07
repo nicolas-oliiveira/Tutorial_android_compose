631b21d96d92ea264e46543de5dea1a391f0ed10
nicolas-oliiveira <nicolas.hassan20@gmail.com>
2022-11-07 09:50:43 -0300

    1

---
+++ app/src/main/java/com/example/aulateste/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.aulateste
+
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("Android")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
+}
